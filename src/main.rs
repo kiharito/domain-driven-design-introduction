0fb812078723d8f758983f80aa009fd7efe05bcc
kiharito <kihara13611.c7d7@gmail.com>
2023-12-24 11:52:34 +0900

    ユーザー名3文字以上の制約追加

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use crate::user::{User, UserName};
 mod user;
 
 fn main() {
-    println!("Hello, {:?}!", User::new(UserName::new("taro")));
+    println!("Hello, {:?}!", User::new(UserName::new("taro").unwrap()));
 }
--- src/user.rs
+++ src/user.rs
@@ -1,3 +1,4 @@
+use anyhow::{ensure, Result};
 use uuid::Uuid;
 
 #[derive(Debug)]
@@ -8,7 +9,10 @@ pub struct User {
 
 impl User {
     pub fn new(name: UserName) -> Self {
-        Self { id: UserId::new(&Uuid::new_v4().to_string()), name }
+        Self {
+            id: UserId::new(&Uuid::new_v4().to_string()),
+            name,
+        }
     }
 }
 
@@ -19,7 +23,12 @@ pub struct UserId {
 
 impl UserId {
     pub fn new(value: &str) -> Self {
-        Self { value: value.to_string() }
+        Self {
+            value: value.to_string(),
+        }
+    }
+    pub fn value(&self) -> String {
+        self.value.clone()
     }
 }
 
@@ -29,7 +38,34 @@ pub struct UserName {
 }
 
 impl UserName {
-    pub fn new(value: &str) -> Self {
-        Self { value: value.to_string() }
+    pub fn new(value: &str) -> Result<Self> {
+        ensure!(value.chars().count() >= 3, "ユーザー名は3文字以上です");
+        Ok(Self {
+            value: value.to_string(),
+        })
+    }
+    pub fn value(&self) -> String {
+        self.value.clone()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_valid_user_name() {
+        let user_name = UserName::new("abc").unwrap();
+        assert_eq!(user_name.value, "abc");
+    }
+
+    #[test]
+    fn test_invalid_user_name() {
+        let result = UserName::new("ab");
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            "ユーザー名は3文字以上です"
+        );
     }
 }
