c2de67cd501848156b5cde5a3cfdafbbc9017ba1
kiharito <kihara13611.c7d7@gmail.com>
2023-12-24 11:21:03 +0900

    directoryを統合

--- cp2/src/main.rs
+++
@@ -1,8 +0,0 @@
-use crate::full_name::FullName;
-
-mod full_name;
-
-fn main() {
-    let full_name = FullName::new("Taro", "Suzuki");
-    println!("Hello, {} {}!", full_name.first_name(), full_name.last_name());
-}
